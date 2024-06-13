07d45ab25b2e4384ddd39e2161440ed51a38d28c
hamimDev <mdhamimhowladerasif@gmail.com>
2024-06-13 06:01:07 +0600

    developed more

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -8,6 +8,7 @@ import { FacultyRoutes } from "../modules/Faculty/faculty.route";
 import { CourseRoutes } from "../modules/Course/course.route";
 import { AdminRoutes } from "../modules/Admin/admin.route";
 import { semesterRegistrationRoutes } from "../modules/semesterRegistration/semesterRegistration.route";
+import { offeredCourseRoutes } from "../modules/OfferedCourse/OfferedCourse.route";
 
 export const router = Router();
 
@@ -47,6 +48,10 @@ const moduleRoutes = [
   {
     path: "/semester-registrations",
     route: semesterRegistrationRoutes,
+  },
+  {
+    path: "/offered-course",
+    route: offeredCourseRoutes,
   },
 ];
 
