ca13ec752f01c56b762d50817a7bc3c2674f3793
hamimDev <mdhamimhowladerasif@gmail.com>
2024-06-25 21:44:32 +0600

    1.User Sign Up is done

--- src/app/modules/users/user.controller.ts
+++ src/app/modules/users/user.controller.ts
@@ -2,7 +2,7 @@ import { sendResponse } from "../../utils/sendResponse";
 import httpStatus from "http-status";
 import { catchAsync } from "../../utils/catchAsync";
 import { UserServices } from "./user.service";
-import { AppError } from "../../errors/AppErrors";
+
 
 const createStudent = catchAsync(async (req, res) => {
   const { password, student: studentData } = req.body;
